84c7bf8efd9ed95d669207e63121c240db46b99a
ishvar99 <ishanvarshney99@gmail.com>
2020-06-04 02:17:15 +0530

    added scripts to package.json

--- backend/server/app.js
+++ backend/server/app.js
@@ -13,5 +13,13 @@ const unsplash = new Unsplash({
 const app = require('express')();
 
 app.get('/api/photos', (req, res) => {
-  unsplash.photos.listPhotos(1, 30).then(toJson);
+  unsplash.photos
+    .listPhotos(1, 30)
+    .then(toJson)
+    .then((data) => res.json(data));
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
