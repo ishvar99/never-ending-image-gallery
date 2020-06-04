0abaea49ea5e5d5af38bfab1de96bd78e8d3c456
ishvar99 <ishanvarshney99@gmail.com>
2020-06-04 14:15:17 +0530

    proxying api requests and fetching data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
-  return <div>Hello World</div>;
+  useEffect(() => {
+    Axios.get('/api/photos').then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+  return (
+    <div className='App'>
+      <h1>IMAGE GALLERY</h1>
+      <div className='image-grid'>
+        <div className='image'>
+          <img src='' />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
