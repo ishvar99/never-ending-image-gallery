333866bc91e9872e05059ef81e6019ffdc19e7dc
ishvar99 <ishanvarshney99@gmail.com>
2020-06-04 16:32:34 +0530

    styling image gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Axios from 'axios';
-
 function App() {
+  const [images, setimages] = useState([]);
   useEffect(() => {
     Axios.get('/api/photos').then((response) => {
       console.log(response.data);
+      setimages(response.data);
     });
   }, []);
   return (
     <div className='App'>
       <h1>IMAGE GALLERY</h1>
       <div className='image-grid'>
-        <div className='image'>
-          <img src='' />
-        </div>
+        {images.map((image) => {
+          return (
+            <div className='image'>
+              <img key={image.id} src={image.urls.thumb} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
